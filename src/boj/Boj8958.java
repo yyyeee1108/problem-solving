91d1b92f8af1489e2e0376b87134797009ff8fd0
Yelim Shin <yyyeee1108@gmail.com>
2026-01-03 03:18:26 +0900

    boj8958/OX퀴즈 refactor
    Scanner -> BufferReader 변경하여 속도 개선
    String -> StringBuilder 변경하여 메모리 효율 개선

--- src/boj/Boj8958.java
+++ src/boj/Boj8958.java
@@ -1,5 +1,8 @@
 package boj;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /**
@@ -9,6 +12,12 @@ import java.util.Scanner;
  */
 public class Boj8958 {
     public static void main(String[] args) {
+        exam1();
+        exam2();
+    }
+
+    private static void exam1() {
+        System.out.println("==이전(일반적인 방식)==");
         Scanner sc = new Scanner(System.in);
 
         /*
@@ -31,6 +40,41 @@ public class Boj8958 {
                 }
             }
             System.out.println(score);
+        }
+    }
+
+    private static void exam2() {
+        System.out.println("==이후(최적화 된 방식)==");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            // 테스트 케이스 개수 받기
+            int N = Integer.parseInt(br.readLine());
+
+            // readLine(): 입력
+            // toCharArray(): 입력받은 문자열을 문자 배열로 변경
+            while (N-- > 0) {
+                char[] rs = br.readLine().toCharArray(); // ['O', 'X', 'O' ...]
+                int score = 0;
+                int stack = 0;
+
+                for (char c : rs) {
+                    if (c == 'O') {
+                        score += ++stack;
+                        // score = score + (++initNum)
+                        // 증감연산자가 사칙연산보다 우선순위
+                    } else {
+                        stack = 0;
+                    }
+                }
+
+                sb.append(score).append("\n");
+            }
+            System.out.println(sb);
+            br.close(); // Scanner처럼 리소스 정리
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
