a0dda04b6aac110277e0ece15a9ad961aa7bb167
Yelim Shin <yyyeee1108@gmail.com>
2026-02-04 14:37:25 +0900

    boj/A+B 1-7
    BR + BW + SB + ST를 이용하여 입출력
    EOF 처리, 다른 구분자 처리

---
+++ src/boj/Boj1000.java
@@ -0,0 +1,29 @@
+package boj;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 문제 이름: A+B
+ * 문제 링크: https://www.acmicpc.net/problem/1000
+ * 풀이 날짜: 2026-02-04
+ */
+public class Boj1000 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        sb.append(a+b);
+        bw.write(sb.toString());
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
---
+++ src/boj/Boj10953.java
@@ -0,0 +1,28 @@
+package boj;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 문제 이름: A+B - 6
+ * 문제 링크: https://www.acmicpc.net/problem/10953
+ * 풀이 날짜: 2026-02-04
+ */
+public class Boj10953 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        while(T-- > 0){
+            StringTokenizer st = new StringTokenizer(br.readLine(),",");
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
